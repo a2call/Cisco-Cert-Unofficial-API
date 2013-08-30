d8120165147e8682b768ebc723cb465819b53849
Mat Wood <mat@thepacketgeek.com>
2013-08-29 23:40:27 -0700

    Support for Heroku dynamic TCP port

--- cisco-cert-api-server.js
+++ cisco-cert-api-server.js
@@ -2,6 +2,8 @@
 var http = require('http');
 var client = require('./cisco-cert-client.js');
 
+var port = process.env.PORT || 3000;
+
 http.createServer(function  (request, response) {
 	client.find(request.url.substr(1), function(certJson) {
 		response.writeHead(200);
@@ -9,4 +11,4 @@ http.createServer(function  (request, response) {
 		response.end();
 		console.log(certJson);
 	});
-}).listen(80);
+}).listen(port);
