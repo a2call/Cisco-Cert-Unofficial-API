92a6f1d43642c0d1acbd4af1593229235eac487b
Mat Wood <mat@thepacketgeek.com>
2013-09-03 08:54:11 -0700

    Added HTTP Error handling
    If code is not found, app now returns an HTTP 404.  If there is a
    server or connection issue from app to www.ciscocertificates.com, app
    now returns an HTTP 500.

--- cisco-cert-client.js
+++ cisco-cert-client.js
@@ -2,23 +2,22 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 exports.find = function(certCode, callback) {
-	// POST verification form and assign to $ object
-	var url = 'http://www.ciscocertificates.com/verify.cfm';
-	request.post(url, {form:{'code':certCode,'fsubmitCheck':'Check'}}, function(err, resp, body) {
-	    if (err)
-	        throw err;
-	    if (resp.statusCode == 200) {
+	// POST verification form and assign callback w/ cert or HTTP error
+	request.post({url: 'http://www.ciscocertificates.com/verify.cfm', timeout: 5000, form:{'code':certCode,'fsubmitCheck':'Check'}}, function(err, resp, body) {
+	    if (err) { 
+	        callback(500);
+	    } else if (resp.statusCode === 200) {
 	    	$ = cheerio.load(body);
 		    
 		    var responseCert = $('b').eq(0).text();
 			var responseName = $('b').eq(1).text();
 
-			var o = {'cert': responseCert, 'name': responseName};
-			callback(JSON.stringify(o));
-		}
+			if (responseCert === 'Check' && responseName === 'Instructions: ') { 
+				callback(404); 
+			} else {
+				var o = {'cert': responseCert, 'name': responseName};
+				callback(JSON.stringify(o));
+			}
+		} else callback(500);
 	});
 }
-
-
-
-
